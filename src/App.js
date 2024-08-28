6351800e32f43d4d1a730718145aabaa76ee0cae
kavyamohan-24 <kavyamohan2005@gmail.com>
2024-08-28 20:59:30 +0530

    shopping cart

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import AddProducts from './components/AddProducts';
+
+function App() {
+  return (
+    <div>
+      <AddProducts/>
+    </div>
+  );
+}
+
+export default App;
